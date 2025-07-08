e0966c150214190185e84142582a453b20b59a92
Rachmat Tri Aji <ajilewo@gmail.com>
2025-07-08 15:17:30 +0700

    Delete ProductNotFoundException.java
    delete

--- ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-public class ProductNotFoundException extends Exception {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-    
-    public ProductNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
